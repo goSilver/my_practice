63adf82620e8186c533f2b75d6881ed8658b8a72
chensh <silverchen961@foxmail.com>
2024-03-03 19:56:05 +0800

    add:stream

--- LanguageFeature/src/main/java/org/example/Main.java
+++ LanguageFeature/src/main/java/org/example/Main.java
@@ -1,11 +1,22 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 /**
  * @author ${USER}
  * @datetime ${DATE} ${TIME}
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Create a list of strings
+        List<String> list = Arrays.asList("a", "b", "c");
+        // Convert the list to a map using a lambda expression
+        Map<String, String> map = list.stream()
+                .collect(Collectors.toMap(s -> s, s -> ""));
+        // Print the map
+        System.out.println(map);
     }
 }
