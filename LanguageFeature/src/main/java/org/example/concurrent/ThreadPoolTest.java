d2e877e85dd8b7d5c43ec0852fa5033b660bfe15
chensh <silverchen961@foxmail.com>
2024-03-05 21:57:10 +0800

    add:ThreadPoolExecutor线程池

--- LanguageFeature/src/main/java/org/example/concurrent/ThreadPoolTest.java
+++ LanguageFeature/src/main/java/org/example/concurrent/ThreadPoolTest.java
@@ -7,6 +7,8 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * 线程池
+ * 不建议使用 Executors 的最重要的原因是：Executors 提供的很多方法默认使用的都是无界的 LinkedBlockingQueue，
+ * 高负载情境下，无界队列很容易导致 OOM，而 OOM 会导致所有请求都无法处理，这是致命问题。所以强烈建议使用有界队列。
  * @author chensh
  * @datetime 2024/3/5 9:46 PM
  */
@@ -17,6 +19,7 @@ public class ThreadPoolTest {
                 1000, TimeUnit.MILLISECONDS,
                 new SynchronousQueue<Runnable>(), Executors.defaultThreadFactory(),
                 new ThreadPoolExecutor.AbortPolicy());
+
         for (int i = 0; i < 3; i++) {
             pool.execute(new ThreadTask());
         }
