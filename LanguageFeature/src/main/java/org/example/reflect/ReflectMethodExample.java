db262e28abba2b8cdd50b5c27a1d50ca74c07e75
chensh <silverchen961@foxmail.com>
2024-03-10 20:08:52 +0800

    add:Method类及其用法

--- LanguageFeature/src/main/java/org/example/reflect/ReflectMethodExample.java
+++ LanguageFeature/src/main/java/org/example/reflect/ReflectMethodExample.java
@@ -1,5 +1,6 @@
 package org.example.reflect;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
@@ -11,7 +12,7 @@ import java.lang.reflect.Method;
  * @datetime 2024/3/10 7:56 PM
  */
 public class ReflectMethodExample {
-    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException {
         Class<?> clazz = Class.forName("org.example.reflect.Circle");
 
         // 根据参数获取public的Method,包含继承自父类的方法
@@ -20,36 +21,62 @@ public class ReflectMethodExample {
         // 获取所有public的方法:
         Method[] methods = clazz.getMethods();
         for (Method method1 : methods) {
-            System.out.println("m::"+ method1.getName());
+            System.out.println("m::" + method1.getName());
         }
 
         System.out.println("=========================================");
 
         // 获取当前类的方法包含private,该方法无法获取继承自父类的method
         Method method1 = clazz.getDeclaredMethod("drawCircle");
-        System.out.println("method1::"+method1);
+        System.out.println("method1::" + method1);
         // 获取当前类的所有方法包含private,该方法无法获取继承自父类的method
-        Method[] methods1=clazz.getDeclaredMethods();
-        for (Method m:methods1){
-            System.out.println("m1::"+m);
+        Method[] methods1 = clazz.getDeclaredMethods();
+        for (Method m : methods1) {
+            System.out.println("m1::" + m);
         }
+
+        System.out.println("=============== invoke() ================");
+
+        Class<?> clazz2 = Class.forName("org.example.reflect.Circle");
+        // 创建对象
+        Circle circle = (Circle) clazz2.newInstance();
+
+        // 获取指定参数的方法对象Method
+        Method methodD = clazz.getMethod("draw", int.class, String.class);
+
+        // 通过Method对象的invoke(Object obj,Object... args)方法调用
+        methodD.invoke(circle, 15, "圈圈");
+
+        // 对私有无参方法的操作
+        Method method3 = clazz.getDeclaredMethod("drawCircle");
+        // 修改私有方法的访问标识
+        method3.setAccessible(true);
+        method3.invoke(circle);
+
+        // 对有返回值得方法操作
+        Method method2 = clazz.getDeclaredMethod("getAllCount");
+        Integer count = (Integer) method2.invoke(circle);
+        System.out.println("count:" + count);
     }
 }
 
 
 class Shape {
-    public void draw(){
+    public void draw() {
         System.out.println("draw");
     }
-    public void draw(int count , String name){
-        System.out.println("draw "+ name +",count="+count);
+
+    public void draw(int count, String name) {
+        System.out.println("draw " + name + ",count=" + count);
     }
 }
-class Circle extends Shape{
-    private void drawCircle(){
+
+class Circle extends Shape {
+    private void drawCircle() {
         System.out.println("drawCircle");
     }
-    public int getAllCount(){
+
+    public int getAllCount() {
         return 100;
     }
 }
