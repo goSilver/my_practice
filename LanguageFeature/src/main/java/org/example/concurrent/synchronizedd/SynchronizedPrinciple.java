d960fe8d55a76f2ab290018c7eb3aa818df2a17c
chensh <silverchen961@foxmail.com>
2024-03-13 22:08:00 +0800

    add:synchronized原理

--- LanguageFeature/src/main/java/org/example/concurrent/synchronizedd/SynchronizedPrinciple.java
+++ LanguageFeature/src/main/java/org/example/concurrent/synchronizedd/SynchronizedPrinciple.java
@@ -25,6 +25,34 @@ package org.example.concurrent.synchronizedd;
  */
 public class SynchronizedPrinciple {
     Object object = new Object();
+
+    /*
+      反编译后：
+      public void method1();
+        descriptor: ()V
+        flags: (0x0001) ACC_PUBLIC
+        Code:
+          stack=2, locals=3, args_size=1
+             0: aload_0
+             1: getfield      #3                  // Field object:Ljava/lang/Object;
+             4: dup
+             5: astore_1
+             6: monitorenter
+             7: aload_1
+             8: monitorexit
+             9: goto          17
+            12: astore_2
+            13: aload_1
+            14: monitorexit
+            15: aload_2
+            16: athrow
+            17: invokestatic  #4                  // Method method2:()V
+            20: return
+          Exception table:
+             from    to  target type
+                 7     9    12   any
+                12    15    12   any
+     */
     public void method1() {
         synchronized (object) {
 
