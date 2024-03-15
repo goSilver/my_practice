4c0325cf7720bb79635018022abe8810af78c3a1
chensh <silverchen961@foxmail.com>
2024-03-15 17:24:27 +0800

    add:使用wait/notify实现线程同步

---
+++ LanguageFeature/src/main/java/org/example/concurrent/locksupport/WaitAndNotifyExample1.java
@@ -0,0 +1,36 @@
+package org.example.concurrent.locksupport;
+
+/**
+ * 使用wait/notify实现线程同步
+ *
+ * @author chensh
+ * @datetime 2024/3/15 5:15 PM
+ */
+public class WaitAndNotifyExample1 extends Thread {
+    @Override
+    public void run() {
+        synchronized (this) {
+            System.out.println("before notify()");
+            notify();
+            System.out.println("after notify()");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        WaitAndNotifyExample1 thread = new WaitAndNotifyExample1();
+        synchronized (thread) {
+            thread.start();
+            // 主线程sleep 3s
+            Thread.sleep(3000);
+            System.out.println("before wait()");
+            thread.wait();  // 阻塞主线程
+            System.out.println("after wait()");
+            /*
+            before wait()
+            before notify()
+            after notify()
+            after wait()
+             */
+        }
+    }
+}
