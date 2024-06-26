de5f361c3903661eb339c53a9de10bbf4ebba166
chensh <silverchen961@foxmail.com>
2024-06-26 18:03:47 +0800

    add:quick sort

--- Algorithm/src/main/java/org/example/sort/QuickSortTest.java
+++ Algorithm/src/main/java/org/example/sort/QuickSortTest.java
@@ -29,23 +29,28 @@ public class QuickSortTest {
     }
 
     int partition(int[] arr, int start, int end) {
+        // 取第一个数为基数
         int pivot = arr[start];
-        int left = start+1;
+        // 从第二个数开始分区
+        int left = start + 1;
+        // 右边界
         int right = end;
-
+        // left、right 相遇时退出循环
         while (left < right) {
+            // 找到第一个大于基数的位置
             while (left < right && arr[left] <= pivot) left++;
+            // 交换这两个数，使得左边分区都小于或等于基数，右边分区大于或等于基数
             if (left != right) {
                 exchange(arr, left, right);
                 right--;
             }
         }
-
+        // 如果 left 和 right 相等，单独比较 arr[right] 和 pivot
         if (left == right && arr[right] > pivot) right--;
         // 将基数和中间数交换
         if (right != start) exchange(arr, start, right);
+        // 返回中间值的下标
         return right;
-
     }
 
     void exchange(int[] arr, int i, int j) {
@@ -93,5 +98,6 @@ public class QuickSortTest {
         System.out.println(JSONUtil.toJsonStr(afterSort));
         assert OrderUtil.isOrderAsc(afterSort);
     }
+
 }
 
