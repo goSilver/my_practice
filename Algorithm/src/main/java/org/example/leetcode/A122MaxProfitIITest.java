43cec9bfca55cc0c62eb826120f6a4c628b95759
chensh <silverchen961@foxmail.com>
2024-08-09 14:30:34 +0800

    add:股票买卖

---
+++ Algorithm/src/main/java/org/example/leetcode/A122MaxProfitIITest.java
@@ -0,0 +1,15 @@
+package org.example.leetcode;
+
+import org.junit.Test;
+
+public class A122MaxProfitIITest {
+    public int maxProfit(int[] prices) {
+        return 0;
+    }
+
+    @Test
+    public void case1() {
+        int[] prices = new int[]{7, 1, 5, 3, 6, 4};
+        assert 7 == maxProfit(prices);
+    }
+}
