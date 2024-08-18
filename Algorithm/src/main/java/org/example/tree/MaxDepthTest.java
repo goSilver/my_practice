f5ad8de2b558b277ed74b7bb86e903d13cfb4424
chensh <silverchen961@foxmail.com>
2024-08-18 13:57:43 +0800

    add:二叉树的最大深度

---
+++ Algorithm/src/main/java/org/example/tree/MaxDepthTest.java
@@ -0,0 +1,46 @@
+package org.example.tree;
+
+import org.example.base.TreeNode;
+import org.junit.Test;
+
+/**
+ * 二叉树的最大深度
+ */
+public class MaxDepthTest {
+    private int res;
+    private int depth;
+
+    private void maxDepth(TreeNode root) {
+        if (root == null)
+            return;
+        depth++;
+        // 到达叶子节点才更新res
+        if (root.getLeft() == null && root.getRight() == null)
+            res = Math.max(res, depth);
+        maxDepth(root.getLeft());
+        maxDepth(root.getRight());
+        depth--;
+    }
+
+    @Test
+    public void case1() {
+        /*
+                1
+           /       \
+          2         5
+         / \       /
+        3   4     6
+                 /
+                7
+         */
+        TreeNode root = new TreeNode(1);
+        root.setLeft(new TreeNode(2));
+        root.setRight(new TreeNode(5));
+        root.getLeft().setLeft(new TreeNode(3));
+        root.getLeft().setRight(new TreeNode(4));
+        root.getRight().setLeft(new TreeNode(6));
+        root.getRight().getLeft().setLeft(new TreeNode(7));
+        maxDepth(root);
+        assert 4 == res;
+    }
+}
---
+++ Algorithm/src/main/java/org/example/tree/TreeNoteBook1.java
@@ -0,0 +1,50 @@
+package org.example.tree;
+
+import org.example.base.ListNode;
+import org.example.base.TreeNode;
+
+/**
+ * 二叉树递归练习
+ */
+public class TreeNoteBook1 {
+
+    /**
+     * 递归一棵二叉树
+     *
+     * @param root 根节点
+     */
+    public void traverse(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        traverse(root.getLeft());
+        traverse(root.getRight());
+    }
+
+    /**
+     * 递归遍历链表
+     *
+     * @param head 头节点
+     */
+    public void traverse(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        traverse(head.next);
+    }
+
+    /**
+     * 递归遍历数组
+     *
+     * @param arr 数组
+     * @param i   访问下标
+     */
+    public void traverse(int[] arr, int i) {
+        if (i == arr.length) {
+            return;
+        }
+        traverse(arr, i + 1);
+    }
+
+
+}
