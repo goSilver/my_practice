2b163559c8085c4878e0fb90dc19cb2de9805811
chensh <silverchen961@foxmail.com>
2024-08-22 21:02:59 +0800

    add:进一步优化空间复杂度

--- Algorithm/src/main/java/org/example/dynamic/FibonacciTest.java
+++ Algorithm/src/main/java/org/example/dynamic/FibonacciTest.java
@@ -19,7 +19,7 @@ public class FibonacciTest {
     }
 
     /**
-     * 带dp数组的，时间复杂度O(n)
+     * 带dp数组的，时间复杂度O(n)，空间复杂度O(n)
      *
      * @param n n
      * @return fib
@@ -44,19 +44,42 @@ public class FibonacciTest {
 
     /**
      * 自底向上的思路
+     * 时间复杂度O(n)，空间复杂度O(n)
      *
      * @param n
      * @return
      */
     public int fibUp(int n) {
-        if (n == 0 ) return 0;
+        if (n == 0) return 0;
         int[] dp = new int[n + 1];
         dp[0] = 0;
         dp[1] = 1;
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i -2];
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
+    }
+
+    /**
+     * 只记录n-1和n-2的数
+     * 进一步优化空间复杂度为O(1)
+     *
+     * @param n
+     * @return
+     */
+    public int fibTwoParam(int n) {
+        if (n == 0) return 0;
+
+        // 定义两个变量记录n-1、n-2
+        int dp_i_1 = 1, dp_i_2 = 0;
+
+        for (int i = 2; i <= n; i++) {
+            int dp_i = dp_i_1 + dp_i_2;
+            // 滚动更新
+            dp_i_2 = dp_i_1;
+            dp_i_1 = dp_i;
+        }
+        return dp_i_1;
     }
 
     @Test
@@ -64,12 +87,16 @@ public class FibonacciTest {
         int res1 = fib(20);
         int res2 = fibWithDp(20);
         int res3 = fibUp(20);
+        int res4 = fibTwoParam(20);
+
 
         System.out.println("result1：" + res1);
         System.out.println("result2：" + res2);
         System.out.println("result3：" + res3);
+        System.out.println("result4：" + res4);
 
         assert res2 == res1;
         assert res3 == res1;
+        assert res4 == res1;
     }
 }
