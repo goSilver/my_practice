58f50d064202dc74d2f66ab730bdfc02c596e16c
chensh <silverchen961@foxmail.com>
2024-08-24 10:21:49 +0800

    add:凑零钱问题，备忘录优化

--- Algorithm/src/main/java/org/example/dynamic/CoinChangeTest.java
+++ Algorithm/src/main/java/org/example/dynamic/CoinChangeTest.java
@@ -2,11 +2,17 @@ package org.example.dynamic;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 /**
  * 凑零钱问题
  */
 public class CoinChangeTest {
+    int[] memo;
+
     public int coinChange(int[] coins, int amount) {
+        memo = new int[amount + 1];
+        Arrays.fill(memo, -666);
         return dp(coins, amount);
     }
 
@@ -14,6 +20,10 @@ public class CoinChangeTest {
         // base case
         if (amount == 0) return 0;
         if (amount < 0) return -1;
+
+        // 备忘录，防止重复计算
+        if (memo[amount] != -666)
+            return memo[amount];
 
         int res = Integer.MAX_VALUE;
         for (int coin : coins) {
@@ -24,12 +34,15 @@ public class CoinChangeTest {
             // 在子问题中寻找最优解，然后+1
             res = Math.min(res, subProblem + 1);
         }
-        return res == Integer.MAX_VALUE ? -1 : res;
+
+        // 把计算结果放回备忘录
+        memo[amount] = (res == Integer.MAX_VALUE ? -1 : res);
+        return memo[amount];
     }
 
     @Test
     public void case1() {
-        int[] coins = new int[]{1,2,5};
+        int[] coins = new int[]{1, 2, 5};
         int result = coinChange(coins, 11);
         System.out.println(result);
         assert result == 3;
